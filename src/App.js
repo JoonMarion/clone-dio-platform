39fe2ca1548627db6fd2b26bda08d0ad0e386e0e
joonmarion <marianojoao52@gmail.com>
2022-10-20 08:54:40 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Feed } from './pages/feed';
-import { Teste } from './pages';
+import { Teste } from './pages/HOOKS/useState';
 
 function App() {
     return (
