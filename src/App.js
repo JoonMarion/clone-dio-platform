429921bde71ccf0b1892656125fb6d63ac62701f
joonmarion <marianojoao52@gmail.com>
2022-10-20 09:24:59 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Feed } from './pages/feed';
 import { Teste } from './pages/HOOKS/useState';
+import { Teste2 } from './pages/HOOKS/useEffect';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/feed" element={<Feed />} />
                 <Route path="/teste" element={<Teste />} />
+                <Route path="/teste2" element={<Teste2 />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/HOOKS/useEffect.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const Teste2 = () => {
+    const [name, setName] = useState('João');
+
+    const handleChangeName = () => {
+        setName((prev) => (prev === 'João' ? 'Maria' : 'João'));
+    };
+
+    useEffect(() => {
+        alert('Nome alterado');
+    }, [name]);
+
+    return (
+        <>
+            <div>
+                <p>{name}</p>
+            </div>
+            <button onClick={handleChangeName}>Trocar o nome</button>
+        </>
+    );
+};
+
+export { Teste2 };
