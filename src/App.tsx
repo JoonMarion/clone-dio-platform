5ba6e8cb3ae6606da5f29e92f5c0d82fcf29b173
joonmarion <marianojoao52@gmail.com>
2022-10-28 09:15:19 -0300

    fix: add register path

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Login } from './pages/login';
 import { Feed } from './pages/feed';
-import { Teste } from './pages/HOOKS/useState';
-import { Teste2 } from './pages/HOOKS/useEffect';
-import { Teste3 } from './pages/HOOKS/useMemo';
-import { Teste4 } from './pages/HOOKS/useCallBack';
+import { Register } from './pages/registration';
 
 function App() {
     return (
@@ -14,10 +11,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/feed" element={<Feed />} />
-                <Route path="/teste" element={<Teste />} />
-                <Route path="/teste2" element={<Teste2 />} />
-                <Route path="/teste3" element={<Teste3 />} />
-                <Route path="/teste4" element={<Teste4 />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
